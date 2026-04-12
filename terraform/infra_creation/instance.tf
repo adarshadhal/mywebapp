4c49702dec0eaedc0c39bb458ce8aba616f9a46c
Your Name <your-email@example.com>
2026-04-12 17:04:04 +0530

    Ami Changes

--- terraform/infra_creation/instance.tf
+++ terraform/infra_creation/instance.tf
@@ -1,6 +1,6 @@
 # Instance Creation
 resource "aws_instance" "my-ec2" {
-    ami = "ami-048f4445314bcaa09"
+    ami = "ami-05d2d839d4f73aafb"
     key_name = "Adarsha2580"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.security-group.id]
